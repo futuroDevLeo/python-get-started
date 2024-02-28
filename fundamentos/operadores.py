bd22c9d48c7e7862a86cbf45b6d827a4f698b1b9
futuroDevLeo <leonunes07@outlook.com>
2024-02-28 18:47:04 -0300

    feat(OPERADORES): continuing study of python basics

---
+++ fundamentos/operadores.py
@@ -0,0 +1,29 @@
+# Operadores Aritméticos
+
+soma = 3 + 2
+print(soma)
+
+subtracao = 3 - 2
+print(subtracao)
+
+multiplicacao = 3 * 2
+print(multiplicacao)
+
+divisao = 3 / 2
+print(divisao)
+
+divisaoInteira = 3 // 2 #retorna a divisão como um número inteiro
+print(divisaoInteira)
+
+modulo = 3 % 2 #retorna o resto da divisão
+print(modulo)
+
+exponenciacao = 3 ** 2
+print(exponenciacao)
+
+
+#Fazendo operações aritméticas
+salario = 1412
+despesas = 893.49
+
+print(salario - despesas)
