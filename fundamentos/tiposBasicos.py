f44af8f9b2d35b95133af23cf1d84e102dda9e95
futuroDevLeo <leonunes07@outlook.com>
2024-02-28 18:18:14 -0300

    feat: initial repo commit

---
+++ fundamentos/tiposBasicos.py
@@ -0,0 +1,29 @@
+# Variaveis e Tipos Básicos
+
+#inteiro
+numero = 5
+print(type(numero))
+
+#ponto flutuante (real)
+outroNumero = 1.2
+print(type(outroNumero))
+
+#string (texto)
+nome = "leo"
+print(type(nome))
+
+#booleano
+print(type(False))
+print(type(True))
+
+#lista
+lista = [1, 2, 3]
+print(type(lista))
+
+#dicionario
+dicionario = {'nome': 'leo', 'idade': 29}
+print(type(dicionario))
+
+#none (sem valor)
+none = None
+print(type(none))
