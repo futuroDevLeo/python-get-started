0ce5b1a951ec826bdc18017e5563aae4b30d7ad6
futuroDevLeo <leonunes07@outlook.com>
2024-04-19 15:58:07 -0300

    refactor(ADS-college): added the statement of exercise 01

--- ADS-college/ex01.py
+++ ADS-college/ex01.py
@@ -1,3 +1,9 @@
+# Exercício 1
+
+# Desenvolva um algoritmo que solicite ao usuário o preço de um produto
+# e um percentual de desconto a ser aplicado a ele. Calcule e exiba o valor
+# do desconto e preço final do produto.
+
 def calculo_de_desconto(preco, percentual):
     desconto = preco * (percentual/100)
     res = preco - desconto
