e8271de8eb59d1eb2be51c4d7ff82d50be87a7e3
futuroDevLeo <leonunes07@outlook.com>
2024-04-19 16:26:04 -0300

    feat(ADS-college): added ex02's answer

--- ADS-college/ex02.py
+++ ADS-college/ex02.py
@@ -5,3 +5,14 @@
 # pelos quais o carro foi alugado. Calcule o preço a pagar, sabendo
 # que o carro custa R$ 60 por dia e R$ 0,15 por km rodado
 
+def calculo_de_preco(km_rodados: int, qtd_de_dias: int) -> float:
+    custo_por_dia = 60
+    custo_por_km = 0.15
+
+    valor_a_pagar = (km_rodados*custo_por_km) + (qtd_de_dias*custo_por_dia)
+    return round(valor_a_pagar, 2)
+
+km = int(input('Quantos km foram percorridos? '))
+dias = int(input('Quantos dias o carro ficou alugado? '))
+
+print(f'O valor a pagar é: {calculo_de_preco(km, dias)}')
