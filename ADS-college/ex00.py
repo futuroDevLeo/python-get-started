2b14682fb302978424dec7c9158ff9c550a6b93d
futuroDevLeo <leonunes07@outlook.com>
2024-04-19 23:29:51 -0300

    feat(ADS-college): added ex00 fixation exercise

---
+++ ADS-college/ex00.py
@@ -0,0 +1,10 @@
+# Exercícios de fixação
+
+# Escreva as seguintes expressões matemáticas em linguagem Python:
+# a) 2 + 3 x 3
+# b) 4² ÷ 3
+# c) (9² + 2) x 6 - 1
+
+print('a) 2 + 3 x 3 =', (2+(3*3)))
+print('b) 4² ÷ 3 =', ((4**2)/3))
+print('c) (9² + 2) x 6 - 1 =', (((9**2+2)*6)-1))
