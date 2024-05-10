ea6e89581f7163e545a4d41740a9be5fd4803675
futuroDevLeo <leonunes07@outlook.com>
2024-05-10 19:22:46 -0300

    feat(FINAL-WORK): question 3 of the final work solved

--- ADS-college/algorithms-final-work/ex03-copiadora.py
+++ ADS-college/algorithms-final-work/ex03-copiadora.py
@@ -23,36 +23,91 @@
 # ♦	Para o adicional de encadernação de capa dura (2) é cobrado um valor extra de 40 reais;
 # ♦	Para o adicional de não querer mais nada (0) é cobrado um valor extra de 0 reais;
 
-
 # O valor final da conta é calculado da seguinte maneira:
 
 # total = (servico * num_pagina) + extra
 
-# Elabore um programa em Python que: 
- 
-# A. Deve-se implementar o print com uma mensagem de boas-vindas que apareça o seu nome;
-# B. Deve-se implementar a função escolha_servico() em que:
-#   a. Pergunta o servico desejado;
-#   b. Retorna o valor servico com base na escolha do usuário;
-#   c. Repete a pergunta do item B.a se digitar uma opção diferente de: dig/ico/ipb/fot;
-# C. Deve-se implementar a função num_pagina() em que:
-#   a. Pergunta o número de páginas;
-#   b. Retorna o número de páginas com desconto seguindo a regra do enunciado
-#       (desconto calculado em cima do número de páginas);
-#   c. Repete a pergunta do item C.a se digitar um valor acima de 20000 ou valor não
-#       numérico (use try/except para não numérico)
-# D. Deve-se implementar a função servico_extra() em que:
-#   a. Pergunta pelo serviço adicional;
-#   b. Retornar o valor de apenas uma das opções de adicional 
-#   c. Repetir a pergunta item D.a se digitar uma opção diferente de: 1/2/0;
-# E. Deve-se implementar o total a pagar no código principal (main), ou seja, não pode estar
-#   dentro de função, conforme o enunciado;
-# F. Deve-se implementar try/except;
-# G. Deve-se inserir comentários relevantes no código;
-# H. Deve-se apresentar na saída de console uma mensagem de boas-vindas com o seu nome;
-# I. Deve-se apresentar na saída de console um pedido no qual o usuário errou a opção de
-#   serviço;
-# J. Deve-se apresentar na saída de console um pedido no qual o usuário digitou ultrapassou
-#   no número de páginas;
-# K. Deve-se apresentar na saída de console um pedido com opção de serviço, número de páginas
-#   e serviço extra válidos;
+# ---------------------------------------Funções---------------------------------------
+
+# Função que mostra os serviços ofertados pela empresa e solicita do usuário a escolha do
+# serviço desejado. Retorna o valor do serviço para cálculos posteriores.
+def escolha_servico():
+    while True:
+        servico = input('Entre com o tipo de serviço desejado\nDIG - Digitação\nICO - Impressão Colorida\nIPB - Impressão Preto e Branco\nFOT - Fotocópia\n>>').upper()
+
+        if servico != 'DIG' and servico != 'ICO' and servico != 'IPB' and servico != 'FOT':
+            print('Escolha inválida, entre com o tipo de serviço novamente')
+            continue
+        else:
+            if servico == 'DIG':
+                return 0.1
+            elif servico == 'ICO':
+                return 1
+            elif servico == 'IPB':
+                return 0.4
+            elif servico == 'FOT':
+                return 0.2
+
+# Função que solicita o número de páginas para calcular o desconto caso ultrapasse as
+# 20 páginas, até 20000, sendo este o limite. Retorna uma mensagem de erro caso ultrapassado.
+# Retorna o número de páginas que não receberão desconto.
+def num_pagina():
+    while True:
+        try:
+            num_pag = int(input('Entre com o número de páginas: '))
+            if num_pag >= 20000:
+                print('Não aceitamos tantas páginas de uma vez.')
+                print('Por favor, entre com o número de páginas novamente.\n')
+                continue
+            else:
+                if num_pag >= 2000 and num_pag < 20000:
+                    num_pag -= round(num_pag*0.25)
+                elif num_pag >= 200:
+                    num_pag -= round(num_pag*0.20)
+                elif num_pag >= 20:
+                    num_pag -= round(num_pag*0.15)
+                else:
+                    num_pag
+                print()
+                return num_pag
+        except ValueError:
+            print('Por favor, entre com um número de páginas valido.\n')
+            continue
+
+# Função que mostra os serviços extras aos clientes e solicita que informe o serviço
+# extra caso desejado, ou 0 caso deseje encerrar o serviço.
+def servico_extra():
+    while True:
+        try:
+            op = int(input(('Deseja adicionar algum servico?\n1 - Encadernação Simples - R$ 15.00\n2 - Encadernação Capa Dura - R$ 40.00\n0 - Não Desejo mais nada\n>>')))
+            if op != 0 and op != 1 and op != 2:
+                print('Opção inválida!\n')
+                continue
+            else:
+                if op == 0:
+                    extra = 0
+                if op == 1:
+                    extra = 15
+                if op == 2:
+                    extra = 40
+                print()
+                return extra
+        except:
+            print('Digite uma opção válida.\n')
+            continue
+
+# ----------------------------------Programa Pincipal----------------------------------
+
+# Programa principal que imprime a mensagem de boas-vindas, chama as outras funções,
+# calcula o valor total do serviço e imprime na tela o resultado.
+print('Bem vindo a Copiadora do Leonardo Nunes\n')
+
+servico = escolha_servico() # Seleção do serviço principal
+
+num_pag = num_pagina() # Seleção do número de páginas
+
+extra = servico_extra() # Seleção de serviços extras, se houver
+
+total = (servico*num_pag) + extra # Cálculo do custo total
+
+print(f'Total: R$ {total:.2f} (serviço: {servico:.2f} * páginas: {num_pag} + extra: {extra:.2f})')
